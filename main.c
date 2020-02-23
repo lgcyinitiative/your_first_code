658fa013784a8848f1e74c9932191cf65d23dfd8
lgcyinitiative <61356813+lgcyinitiative@users.noreply.github.com>
2020-02-22 19:29:19 -0600

    Update main.c
    yeah yeah

--- main.c
+++ main.c
@@ -7,3 +7,26 @@ int main()
   }
   return 0;
 }
+int main()
+{
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf("Hello Holberton\n");
+  }
+  return 0;
+}
+int main()
+{
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf("Hello Holberton\n");
+  }
+  return 0;
+}
+int main()
+{
+  int i;
+  for (i = 0; i < 10; i++)
+  {
